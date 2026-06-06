134463ac6869e18336d76a9ac0f6b5aa71585c69
manjusharami <manjusha84r2020@gmail.com>
2026-06-07 00:17:22 +0100

    add the message

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/manjusharami/microServiceGo
 
 go 1.23.2
+
+require github.com/go-chi/chi/v5 v5.3.0
--- main.go
+++ main.go
@@ -3,17 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"chi"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
  func HandleFuncs(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hellow world")
 }
 
 func main() {
+	router:=chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/hello",HandleFuncs)
+
 
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(HandleFuncs),
+		Handler: router,
 	}
 
 	fmt.Println("server is runing")
